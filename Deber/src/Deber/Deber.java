95a11ad4db8f7fba239fb2d0066366575cf1f06a
PabloVall <84852018+PabloVall@users.noreply.github.com>
2021-06-07 01:24:38 -0500

    Add files via upload

---
+++ Deber/src/Deber/Deber.java
@@ -0,0 +1,41 @@
+package Deber;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Pablo
+ */
+public class Deber {
+
+    /**
+     * @param args the command line arguments
+     */
+    private Scanner teclado;
+    public static void main(String[] args) {
+        //while' tablas de multiplicar
+        int contador = 1 ;
+        System.out.println("While");
+        int numero=8;
+        while (contador<=12){
+            int resultado=contador*numero;
+            System.out.println(numero+"*"+contador+"="+resultado);
+            contador++; // igual a contador=contador +1
+        } 
+        //do while
+        System.out.println("do while");
+        int contador2=12;
+        do{
+            int resultado=contador2*numero;
+            System.out.println(numero+"*"+contador2+"="+resultado);
+            contador2 -=1; //contador2=contador2-1
+        }while(contador2>=1);
+        //For -tabla de multiplicar
+        System.out.println("for");
+        for(int contador3=1;contador3<=12;contador3++){
+            int resultado=numero*contador3;
+            System.out.println(numero+"*"+contador3+"="+resultado);
+        }
+    }
+    
+}
