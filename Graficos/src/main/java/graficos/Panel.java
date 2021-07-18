e61cc68f5d43ea06bc6352ee7afb3f6796f2a8f2
PabloVall <84852018+PabloVall@users.noreply.github.com>
2021-07-18 13:23:08 -0500

    Add files via upload

---
+++ Graficos/src/main/java/graficos/Panel.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graficos;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author valle
+ */
+public class Panel extends JPanel{
+    private Figure figura;
+    private int tipoFigura;
+    
+    public Panel(){
+        setBounds(0, 0, 500, 500);
+        figura= new Figure(0, 0, 200, 200);
+        tipoFigura = 0;
+    }
+    
+    @Override
+    public void paint(Graphics lienzo){
+        //lienzo.clipRect(0, 0, 500, 500);
+        lienzo.setColor(Color.GRAY);
+        switch (tipoFigura){
+            case 1:
+                lienzo.drawLine(figura.getX(), figura.getY(), figura.getAncho(), figura.getAlto());
+                break;
+            case 2:
+                lienzo.drawRect(figura.getX(), figura.getY(), figura.getAncho(), figura.getAlto());
+                break;
+            case 3:
+                lienzo.drawRoundRect(20, 50, 300, 200, 50, 30);
+                break;
+            case 4:
+                lienzo.drawOval(figura.getX(), figura.getY(), figura.getAncho(), figura.getAlto());
+                break;
+        }
+    }
+
+    public Figure getFigura() {
+        return figura;
+    }
+
+    public void setFigura(Figure figura) {
+        this.figura = figura;
+    }
+
+    public int getTipoFigura() {
+        return tipoFigura;
+    }
+
+    public void setTipoFigura(int tipoFigura) {
+        this.tipoFigura = tipoFigura;
+    }
+
+}
